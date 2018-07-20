40517bebd94c75b684dc1666507d213a7e2a5a45
Andres Contreras <andrescontreras@Andress-MacBook-Air.local>
2018-07-19 23:06:37 -0700

    Adding Footer to other sections. Fixed a few issues with community service section with the description not being centered. Added a small JavaScript function that alerts user that the place they're clicking is still still under development.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Define the application entry point
+function myFunction() {
+    //Change here..
+	alert("Functionality not available.");
+    return;
+}
+
+// Start the application
+// Add in arguments in MyFunction()
