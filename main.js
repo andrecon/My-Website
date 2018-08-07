40bf160d2ac719e74317857445c495fc357c9d30
Andres Contreras <andrescontreras@Andress-MacBook-Air.local>
2018-08-06 18:56:19 -0700

    Fixing Education section

--- main.js
+++ main.js
@@ -8,12 +8,3 @@ function myFunction() {
 
 // Start the application
 // Add in arguments in MyFunction()
-
-function FullScreenMenu(id) {
-  var element = document.getElementById(id);       
-  if (element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullScreen) {
-    element.webkitRequestFullScreen();
-  }  
-}
