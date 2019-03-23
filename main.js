ac76bb38b48a2e10cb611a004fba418a13fde9b8
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-03-23 16:49:41 -0700

    Fixed some spelling, styling and indentation

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
 
 // Define the application entry point
 function myFunction() {
-    //Change here..
+	//Change here..
 	alert("Functionality not available.");
-    return;
-}
-
-// Start the application
-// Add in arguments in MyFunction()
-
-function FullScreenMenu(id) {
-  var element = document.getElementById(id);       
-  if (element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullScreen) {
-    element.webkitRequestFullScreen();
-  }  
+	return;
 }
